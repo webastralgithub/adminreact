a7f71b8b31943aa3524f17cefac53a59810a8582
webastral <webastral.development@gmail.com>
2023-12-28 16:10:36 +0530

    application setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Child from './Components/Child';
+
+const App=()=> {
+  return (
+    <div className="App">
+     <h1>HELLO WORLD</h1>
+     <Child/>
+    </div>
+  );
+}
+
+export default App;
